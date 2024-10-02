08fa718d457233399eb172b67568ea44a5113c0c
frbarbre <fba@conversio.dk>
2024-10-02 13:55:30 +0200

    fixed ts config

--- src/lib/mysql-client.ts
+++ src/lib/mysql-client.ts
@@ -1,10 +1,6 @@
 import { getEnv } from "waku";
 import mysql, { ConnectionOptions } from "mysql2/promise";
 
-interface MySQLClient extends mysql.Connection {
-  query: (sql: string) => Promise<[any, any]>;
-}
-
 const access: ConnectionOptions = {
   user: getEnv("DB_USER")!,
   password: getEnv("DB_PASSWORD")!,
@@ -12,6 +8,6 @@ const access: ConnectionOptions = {
   port: 3306,
   host: getEnv("DB_HOST")!,
 };
-export async function mySQLClient(): Promise<MySQLClient> {
-  return (await mysql.createConnection(access)) as MySQLClient;
+export async function mySQLClient() {
+  return await mysql.createConnection(access);
 }
